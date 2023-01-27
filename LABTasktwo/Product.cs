6230bfce07af2a2df060366f7ef8cf392dd095f7
Vugarismayil <121168237+Vugarismayil@users.noreply.github.com>
2023-01-27 17:34:58 +0400

    lab

---
+++ LABTasktwo/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABTasktwo
+{
+    internal class Product
+    {
+        public string Name;
+        public int Price;
+        public int Count;
+        public int Income;
+
+        public Product(string name, int price, int count)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Count = count;
+        }
+
+        public void Sell()
+        {
+            if (Count > 0)
+            {
+                Count--;
+                Income += Price;
+            }
+            else
+            {
+                Console.WriteLine("Mehsul Bitmisdir");
+            }
+            
+
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nPrice: {Price}\nCount: {Count}\nIncome: {Income}");
+            
+        }
+    }
+
+
+}
---
+++ LABTasktwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace LABTasktwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Laptop", 10, 50);
+
+            product1.Sell();
+            product1.Sell();
+            product1.Sell();
+
+            product1.ShowInfo();
+
+
+
+
+
+
+        }
+    }
+}
